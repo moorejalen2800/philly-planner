5d1cf079bf21c07ed5d450c584fc5962e39cd179
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-24 13:17:12 -0400

    Created utils folder to house password verification methods, updated server package.json with relevant dependencies, created related resolvers for login and user creation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,16 +5,43 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return await User.find(params).populate('outings');
+    user: async (parent, { name }) => {
+      return User.findOne({ name }).populate('thoughts');
     },
-    outings: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    outings: async (parent, { name }) => {
+      const params = name ? { name } : {};
+      return Outing.find(params).sort({created_at: -1});
     },
+    outing: async (parent, { outingId }) => {
+      return Outing.findOne({_id: outingId})
+    }
+
   },
   Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    
+    
     createMatchup: async (parent, args) => {
       const matchup = await Matchup.create(args);
       return matchup;
