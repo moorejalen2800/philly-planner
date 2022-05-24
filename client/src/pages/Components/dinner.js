a984218fe714d1c08d5a6c36d02609cfca48b3e7
Kgiunta <giunta.kyle@yahoo.com>
2022-05-24 14:31:04 -0400

    premerge

--- client/src/pages/Components/dinner.js
+++ client/src/pages/Components/dinner.js
@@ -9,7 +9,7 @@ const cors = "https://cors-anywhere.herokuapp.com/";
 async function getApi(budget, dinnerOption) {
   var requestUrl =
     cors +
-    `https://api.yelp.com/v3/businesses/search?latitude=39.9526&longitude=-75.1652&price=${budget}&categories=${dinnerOption}`;
+    `https://api.yelp.com/v3/businesses/search?latitude=39.9526&longitude=-75.1652&price=${budget}&categories=${dinnerOption}&radius=5000`;
 
   // `https://api.yelp.com/v3/businesses/search?latitude=39.9526&longitude=-75.1652&price=${resPrice}&categories=${resActivity}`;
 
@@ -48,64 +48,103 @@ const Dinner = () => {
 
     setFormState({ ...formState, [name]: value });
   };
+
+  // const searchForm = (query) =>
+  // dinnerOption.search(query)
+  //   .then((res) => setFormState(res.data))
+  //   .catch((err) => console.log(err));
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    getApi(formState.budget, formState.dinnerOption);
+  };
+
+  // user names event date
+  // populate calendar and select date
+  // fill out budgert and ethiccnic food
+  // needs banner at top with login logout info
   return (
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-      }}
-      className="card card-rounded w-50"
-    >
-      <div className="card-header bg-dark text-center">
-        <h1>Plan The Perfect Philly Night!</h1>
-      </div>
-      <form>
-        <div className="card-body m-5">
-          <label>Select Budget Range</label>
-          <select
-            name="budget"
-            value={formState.budget}
-            onChange={handleChange}
-          >
-            {" "}
-            <option value="1">$</option>
-            <option value="2">$$</option>
-            <option value="3">$$$</option>
-            <option value="4">$$$$</option>
-          </select>
-
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ul className="square">
-              {matchupList.map((matchup) => {
-                return (
-                  <li key={matchup._id}>
-                    <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                      {matchup.tech1} vs. {matchup.tech2}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
+    <div id="container">
+      <div id="calendar" className="card card-rounded w-50">
+        <div className="card-header bg-dark text-center">
+          <h1>Plan The Perfect Philly Night!</h1>
         </div>
-      </form>
-      <div class="card-body m-5">
-        <label>Whatcha Feelin' For Dinner?</label>
-        <br></br>
-        <input
-          onChange={handleChange}
-          name="dinnerOption"
-          value={formState.dinnerOption}
-          placeholder="Italian, Mexican, etc.."
-        ></input>
+        <div class="card-body m-5">
+          <label>Whatcha Feelin' For Dinner?</label>
+          <br></br>
+          <input
+            onChange={handleChange}
+            name="dinnerOption"
+            value={formState.dinnerOption}
+            placeholder="Italian, Mexican, etc.."
+          ></input>
+        </div>
+
+        <div className="card-footer text-center m-3">
+          <h2>Ready to Move To The Next Step?</h2>
+          {/* <Link to="/matchup"> */}
+          <button onClick={handleFormSubmit} className="btn btn-lg btn-danger">
+            Plan Your Next Step
+          </button>
+          {/* </Link> */}
+        </div>
       </div>
 
-      <div className="card-footer text-center m-3">
-        <h2>Ready to Move To The Next Step?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Plan Your Next Step</button>
-        </Link>
+      <div id="dinner" className="card card-rounded w-50">
+        <div className="card-header bg-dark text-center">
+          <h1>Plan The Perfect Philly Night!</h1>
+        </div>
+        <form>
+          <div className="card-body m-5">
+            <label>Select Budget Range</label>
+            <select
+              name="budget"
+              value={formState.budget}
+              onChange={handleChange}
+            >
+              {" "}
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+            </select>
+
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <ul className="square">
+                {matchupList.map((matchup) => {
+                  return (
+                    <li key={matchup._id}>
+                      <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                        {matchup.tech1} vs. {matchup.tech2}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+        </form>
+        <div class="card-body m-5">
+          <label>Whatcha Feelin' For Dinner?</label>
+          <br></br>
+          <input
+            onChange={handleChange}
+            name="dinnerOption"
+            value={formState.dinnerOption}
+            placeholder="Italian, Mexican, etc.."
+          ></input>
+        </div>
+
+        <div className="card-footer text-center m-3">
+          <h2>Ready to Move To The Next Step?</h2>
+          {/* <Link to="/matchup"> */}
+          <button onClick={handleFormSubmit} className="btn btn-lg btn-danger">
+            Plan Your Next Step
+          </button>
+          {/* </Link> */}
+        </div>
       </div>
     </div>
   );
